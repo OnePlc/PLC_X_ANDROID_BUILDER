c328adf9adfae321935d1bb802b96afaa6b18712
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2021-03-19 19:36:11 +0100

    installer

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# PLC_X_ANDROID_BUILDER
-Build Config Files and Assets for Android Apps in onePlace
+# oneplace-android-builder
+
+## Introduction
+
+This is the Wizard to Build Resource Packs for Android Apps in onePlace.
+
+
+## Installation
+
+The easiest way to install onePlace Android Builder is via composer
+```shell script
+composer require oneplace/oneplace-android-builder
+```
--- data/install.sql
+++ data/install.sql
@@ -2,4 +2,15 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+--
+-- Basic Permissions
+--
+INSERT INTO `permission` (`permission_key`, `module`, `label`, `nav_label`, `nav_href`, `show_in_menu`, `needs_globaladmin`) VALUES
+('index', 'OnePlace\\AndroidBuilder\\Controller\\WizardController', 'Overview', 'Android Apps', '/android/builder', 1, 0);
+
+--
+-- Nav Icon
+--
+INSERT INTO `settings` (`settings_key`, `settings_value`) VALUES ('android-builder-icon', 'fab fa-android');
+
 COMMIT;
